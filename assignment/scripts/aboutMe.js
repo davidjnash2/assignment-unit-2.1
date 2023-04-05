e28d8ffeef72cdcb0360440c76492a606bf19800
Dave Nash <davidjnash2@gmail.com>
2023-04-05 17:11:20 -0500

    continued script additions

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -10,7 +10,7 @@ console.log( lastName );
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 let fullName = firstName + ' ' + lastName;
-console.log ( fullName );
+console.log( fullName );
 
 // 4 - Console log the value of `fullName`
 console.log( 'My full name is', fullName );
@@ -18,7 +18,7 @@ console.log( 'My full name is', fullName );
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 let luckyNumber = 9;
-console.log ( luckyNumber );
+console.log( luckyNumber );
 
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
@@ -27,20 +27,38 @@ console.log( 'My name is ' + fullName + ', and I think ' + luckyNumber + ' is a 
 
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
+let adventurous = false;
+console.log( adventurous );
 
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
+let food = 'bone marrow'
+console.log( food );
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
+let pets = 0;
+console.log( pets );
 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
+let friendsPets = 112358;
+console.log( friendsPets );
 
 // 11 - Add two pets to your `pets` variable
+pets += 2;
+console.log( pets );
+
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
+let allowedPets = -45;
+console.log( allowedPets );
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
+if ( adventurous ){
+    console.log ('Adventures are great!');
+}
+else{
+    console.log( 'How about we stay home?');
+} // end trip status
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
