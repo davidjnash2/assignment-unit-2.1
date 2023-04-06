5f5dcd1d949b345d53fa35c0ad3fbd2f1058077c
Dave Nash <davidjnash2@gmail.com>
2023-04-06 14:32:48 -0500

    added name info to console log parantheticals)

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,16 +1,16 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'Dave';
-console.log( firstName );
+console.log( 'firstName:', firstName );
 
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'Nash';
-console.log( lastName );
+console.log( 'lastName:', lastName );
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 let fullName = firstName + ' ' + lastName;
-console.log( fullName );
+console.log( 'fullName:', fullName );
 
 // 4 - Console log the value of `fullName`
 console.log( 'My full name is', fullName );
@@ -18,7 +18,7 @@ console.log( 'My full name is', fullName );
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 let luckyNumber = 9;
-console.log( luckyNumber );
+console.log( 'luckyNumber:', luckyNumber );
 
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
@@ -28,28 +28,28 @@ console.log( 'My name is ' + fullName + ', and I think ' + luckyNumber + ' is a 
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 let adventurous = true;
-console.log( adventurous );
+console.log( 'adventurous:', adventurous );
 
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 let food = 'bone marrow'
-console.log( food );
+console.log( 'food:', food );
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
 let pets = 7;
-console.log( pets );
+console.log( 'pets:', pets );
 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 let friendsPets = 12;
-console.log( friendsPets );
+console.log( 'friendsPets:', friendsPets);
 
 // 11 - Add two pets to your `pets` variable
 pets += 2;
-console.log( pets );
+console.log( 'pets:', pets );
 
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 let allowedPets = 2;
-console.log( allowedPets );
+console.log( 'allowedPets:', allowedPets );
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
@@ -93,7 +93,8 @@ else if( pets > allowedPets ){
 // console.log `mostPets` after the conditional has run.
 let mostPets = pets > friendsPets ? pets : friendsPets;
 let leastPets = pets < friendsPets ? pets : friendsPets;
-console.log ( mostPets )
+console.log ( 'mostPets:', mostPets );
+console.log ( 'leastPets:', leastPets );
 if( pets < friendsPets ){
     console.log( 'Mike has so many pets! ' + mostPets + ', which is way more than my paltry ' + leastPets + '.');
  }
