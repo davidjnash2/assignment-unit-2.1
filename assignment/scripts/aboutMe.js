a092e51cc54f0cdc4f40de01ebd8a19c187ec8d7
Dave Nash <davidjnash2@gmail.com>
2023-04-06 17:09:27 -0500

    revising question 16 to conditional statement and answering more questions

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,23 +1,23 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = 'Dave';
+let firstName = 'Dave'; //assigning first name
 console.log( 'firstName:', firstName );
 
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-let lastName = 'Nash';
+let lastName = 'Nash'; //assigning last name
 console.log( 'lastName:', lastName );
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-let fullName = firstName + ' ' + lastName;
-console.log( 'fullName:', fullName );
+let fullName = firstName + ' ' + lastName; //creating value for full name
 
 // 4 - Console log the value of `fullName`
+console.log( 'fullName:', fullName );
 console.log( 'My full name is', fullName );
 
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-let luckyNumber = 9;
+let luckyNumber = 9; //assigning lucky number
 console.log( 'luckyNumber:', luckyNumber );
 
 // 6 - Console log this sentence, adding in the variables you created above: 
@@ -27,28 +27,28 @@ console.log( 'My name is ' + fullName + ', and I think ' + luckyNumber + ' is a 
 
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-let adventurous = true;
+let adventurous = true; //I'm outdoorsy
 console.log( 'adventurous:', adventurous );
 
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-let food = 'bone marrow'
+let food = 'bone marrow' //assigning favorite food
 console.log( 'food:', food );
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-let pets = 7;
+let pets = 110; // assigning pets
 console.log( 'pets:', pets );
 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-let friendsPets = 12;
+let friendsPets = 12; //Mike's pretend pets
 console.log( 'friendsPets:', friendsPets);
 
 // 11 - Add two pets to your `pets` variable
-pets += 2;
+pets += 2; //adopting 2 pets
 console.log( 'pets:', pets );
 
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-let allowedPets = 2;
+let allowedPets = 2; //landlords r mean
 console.log( 'allowedPets:', allowedPets );
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
@@ -91,10 +91,20 @@ else if( pets > allowedPets ){
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
-let mostPets = pets > friendsPets ? pets : friendsPets;
-let leastPets = pets < friendsPets ? pets : friendsPets;
+let mostPets;
+if( pets > friendsPets ){ mostPets = pets;
+}
+else if( friendsPets > pets ){mostPets = friendsPets;
+}
 console.log ( 'mostPets:', mostPets );
+
+let leastPets;
+if( pets < friendsPets ){ leastPets = pets;
+}
+else if( friendsPets < pets ){ leastPets = friendsPets;
+}
 console.log ( 'leastPets:', leastPets );
+
 if( pets < friendsPets ){
     console.log( 'Mike has so many pets! ' + mostPets + ', which is way more than my paltry ' + leastPets + '.');
  }
@@ -112,7 +122,20 @@ if( pets < friendsPets ){
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
 
-
+switch ( luckyNumber ) {
+    case 1:
+        console.log('First is the worst!');
+        break;
+    case 2:
+        console.log( 'Second is the best!' );
+        break;
+    case 3: 
+        console.log( 'Third is the one iwth the polka dot dress! ');
+        break;
+    default:
+        console.log( 'Luck is what happens when preparation meets opportunity.');
+    
+}
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
